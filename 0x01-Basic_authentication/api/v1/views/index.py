49fb25b9d51ce6f5c4612e75f5f1dfe181d5046a
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-09-05 00:21:48 +0000

    task1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,9 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+app_views = Blueprint('app_views', __name__)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -24,3 +27,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized():
+    """Endpoint to trigger a 401 error"""
+    abort(401)
+
