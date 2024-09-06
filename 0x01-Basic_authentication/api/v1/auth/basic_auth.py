8dceb3a3ffc4d0198dc95c0d2bb6038104cf4a43
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-09-06 10:16:13 +0000

    task9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,27 @@ class BasicAuth(Auth):
 
         # Return the part after 'Basic '
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 part of the Authorization header.
+
+        Args:
+            base64_authorization_header (str): The Base64 string to decode.
+
+        Returns:
+            str: The decoded string, or None if invalid Base64.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            # Decode the Base64 string and convert it to a UTF-8 string
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            # Return None if there's an error in decoding
+            return None
